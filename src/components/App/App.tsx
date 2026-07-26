c6b69226bb05df877a096626a652442effead10c
konorezzova-spec <konorezzova@gmail.com>
2026-07-26 20:25:08 +0300

    it's working locally

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,37 +1,29 @@
 import "./App.module.css";
 import { useState } from "react";
 import SearchBar from "../SearchBar/SearchBar.tsx";
-import toast, { Toaster } from "react-hot-toast";
 import { fetchMovies } from "../../services/movieService.ts";
 import type { Movie } from "../../types/movie.ts";
 import MovieGrid from "../MovieGrid/MovieGrid.tsx";
-import ErrorMessage from "../ErrorMessage/ErrorMessage.tsx";
-import Loader from "../Loader/Loader.tsx";
 import MovieModal from "../MovieModal/MovieModal.tsx";
-// import css from "../MovieModal/MovieModal.module.css";
+import ReactPaginate from "./ReactPaginate";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 
 export default function App() {
-  // Оголошуємо і типізуємо стан завантаження фільмів
-  const [movies, setMovies] = useState<Movie[]>([]);
-  // Додаємо стан індикатора завантаження
-  const [isLoading, setIsLoading] = useState(false);
-  // Оголошуємо стан помилки
-  const [isError, setIsError] = useState(false);
+  const [query, setQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const handleSearch = async (query: string) => {
-    try {
-      setIsLoading(true);
-      setIsError(false);
-      const data = await fetchMovies(query);
-      setMovies(data);
-      if (data.length == 0) {
-        toast("No movies found for your request.");
-      }
-    } catch {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
+  const { data, isLoading, isError, isSuccess } = useQuery({
+    queryKey: ["movies", query, currentPage],
+    queryFn: () => fetchMovies(query, currentPage),
+    enabled: query !== "",
+    placeholderData: keepPreviousData,
+  });
+
+  const totalPages = data?.total_pages ?? 0;
+
+  const handleSearch = async (newQuery: string) => {
+    setQuery(newQuery);
+    setCurrentPage(1);
   };
 
   // for Modal
@@ -49,10 +41,19 @@ export default function App() {
   return (
     <>
       <SearchBar onSubmit={handleSearch} />
-      {isLoading && <Loader />}
-      {isError && <ErrorMessage />}
-      {movies.length > 0 && <MovieGrid onSelect={openModal} movies={movies} />}
-      <Toaster />
+      {isSuccess && totalPages > 1 && (
+        <ReactPaginate
+          totalPages={totalPages}
+          currentPage={currentPage}
+          setPage={setCurrentPage}
+        />
+      )}
+
+      {isLoading && <p>Loading data, please wait...</p>}
+      {isError && <p>Whoops, something went wrong! Please try again!</p>}
+      {data && data.total_pages > 0 && (
+        <MovieGrid onSelect={openModal} movies={data.results} />
+      )}
 
       {/* Modal */}
       {isModalOpen && selectedItem && (
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -4,18 +4,23 @@ import { type Movie } from "../types/movie";
 // Типізували відповідь від бекенду
 interface MovieHttpResponse {
   results: Movie[];
+  total_pages: number;
+  page: number;
 }
 const myKey = import.meta.env.VITE_TMDB_TOKEN;
 
 // HTTP-функція запиту статей
-export const fetchMovies = async (query: string): Promise<Movie[]> => {
+export const fetchMovies = async (
+  query: string,
+  page: number
+): Promise<MovieHttpResponse> => {
   const url = "https://api.themoviedb.org/3/search/movie";
 
   const params = {
     query: query,
     include_adult: false,
     language: "en-US",
-    page: 1,
+    page,
   };
   const headers = {
     accept: "application/json",
@@ -24,6 +29,5 @@ export const fetchMovies = async (query: string): Promise<Movie[]> => {
 
   const response = await axios.get<MovieHttpResponse>(url, { params, headers });
 
-  // console.log("search log", response.data.results);
-  return response.data.results;
+  return response.data;
 };
